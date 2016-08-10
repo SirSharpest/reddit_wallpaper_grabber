edeeb6f245a6dd8fec7aa27b4c80ad5deb9b6efe
= <=>
2016-08-10 12:24:37 +0100

    Changed to be more function based

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import requests
 import re
 import sys
 import os
-import random
+# import random this is unused until we fix logic
 import datetime
+import subprocess
 
 
 def modification_date(filename):
@@ -16,44 +17,38 @@ def modification_date(filename):
     return datetime.datetime.fromtimestamp(t)
 
 
-reddit = praw.Reddit(user_agent='wallpaper_grabber')
-submissions = reddit.get_subreddit('wallpapers').get_hot(limit=25)
-filter_name = "^(.*[\\\/])"  # This removes everything but after the last /
-image_names = []  # Container for names' of images
+def download_x_pictures(num_to_get):
+    reddit = praw.Reddit(user_agent='wallpaper_grabber')
+    submissions = reddit.get_subreddit('wallpapers').get_hot(limit=num_to_get)
+    filter_name = "^(.*[\\\/])"  # This removes everything but after the last /
+    image_names = []  # Container for names' of images
+
+    for img in submissions:
+        if "JPG" in img.url.upper() or "PNG" in img.url.upper():
+            try:
+                filename = "wallpapers/" + re.sub(filter_name, '', img.url)
+                file = requests.get(img.url)
+                with open(filename, 'wb') as image:
+                    image.write(file.content)
+                    image_names.append(filename)
+            except requests.exceptions as e:
+                sys.stderr.write("HTTPs Problem with: " + img.url + ' ')
+
+    return image_names
 
 
-if os.path.exists("wallpapers"):
-    date = modification_date("wallpapers")
-    age = (datetime.datetime.now() - date).seconds
-    if age / 3600 >= 24:
-        import shutil
-        shutil.rmtree("wallpapers")
-else:
-    os.makedirs("wallpapers")
+def check_file_exits():
+    if os.path.exists("wallpapers"):
+        date = modification_date("wallpapers")
+        age = (datetime.datetime.now() - date).seconds
+        if age / 3600 >= 24:
+            import shutil
+            shutil.rmtree("wallpapers")
+        else:
+            os.makedirs("wallpapers")
 
-# Loops through all submissions and decides if they're valid image formats
-# Proceeds to download them if so!
-for img in submissions:
-    if "JPG" in img.url.upper() or "PNG" in img.url.upper():
-        try:
-            filename = "wallpapers/" + re.sub(filter_name, '', img.url)
-            file = requests.get(img.url)
 
-            with open(filename, 'wb') as image:
-                image.write(file.content)
-
-            image_names.append(filename)
-        except requests.exceptions as e:
-            sys.stderr.write("HTTPs Problem with: " + img.url + ' ')
-
-# If there is no images downloaded then exit!
-if(len(image_names) < 1):
-    sys.ext()
-else:
-    background = random.choice(image_names)
+def set_wallpaper(wallpaper_to_use):
     command = "gsettings set org.gnome.desktop.background picture-uri\
-    file:" + os.getcwd() + "/" + background
-
-    import subprocess
-    process = subprocess.Popen(command.split())
-    sys.exit()
+    file:" + os.getcwd() + "/" + wallpaper_to_use
+    subprocess.Popen(command.split())
